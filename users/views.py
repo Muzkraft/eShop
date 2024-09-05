99bf8c353b3234fe4cb4fe301c500b1affc01928
Muzkraft <no1z555@gmail.com>
2024-09-05 22:59:46 +0500

    Add to cart view, refactor included cart templates

--- users/views.py
+++ users/views.py
@@ -36,7 +36,8 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'Добро пожаловать {username}, будьте как дома!')
                 
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
                 return HttpResponseRedirect(reverse("main:index"))
