9a5f8734bf993d75f27eb113ce4f223180846ce1
Muzkraft <no1z555@gmail.com>
2024-09-23 23:26:20 +0500

    add orders list in profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib import auth, messages
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -13,15 +15,21 @@ def registration(request):
         if form.is_valid():
             form.save()
 
-            session_key = request.session.session_key # запоминаем ключ сессии еще не авторизованного пользователя
-             
-            user = form.instance 
+            session_key = (
+                request.session.session_key
+            )  # запоминаем ключ сессии еще не авторизованного пользователя
+
+            user = form.instance
             auth.login(request, user)
-            
+
             if session_key:
-                Cart.objects.filter(session_key=session_key).update(user=user) # обновляем по ключу сессии корзину пользователя продуктами, которые он положил будучи не авторизованным
-                    
-            messages.success(request, f'Мы рады знакомству {user.username}, проходите пожалуйста!')
+                Cart.objects.filter(session_key=session_key).update(
+                    user=user
+                )  # обновляем по ключу сессии корзину пользователя продуктами, которые он положил будучи не авторизованным
+
+            messages.success(
+                request, f"Мы рады знакомству {user.username}, проходите пожалуйста!"
+            )
             return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
@@ -40,20 +48,26 @@ def login(request):
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
-            
-            session_key = request.session.session_key # запоминаем ключ сессии еще не авторизованного пользователя
-            
+
+            session_key = (
+                request.session.session_key
+            )  # запоминаем ключ сессии еще не авторизованного пользователя
+
             if user:
                 auth.login(request, user)
-                messages.success(request, f'Добро пожаловать {username}, будьте как дома!')
-                
+                messages.success(
+                    request, f"Добро пожаловать {username}, будьте как дома!"
+                )
+
                 if session_key:
-                    Cart.objects.filter(session_key=session_key).update(user=user) # обновляем по ключу сессии корзину пользователя продуктами, которые он положил будучи не авторизованным
-                
-                redirect_page = request.POST.get('next', None)
-                if redirect_page and redirect_page != reverse('user:logout'):
-                    return HttpResponseRedirect(request.POST.get('next'))
-                
+                    Cart.objects.filter(session_key=session_key).update(
+                        user=user
+                    )  # обновляем по ключу сессии корзину пользователя продуктами, которые он положил будучи не авторизованным
+
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
+                    return HttpResponseRedirect(request.POST.get("next"))
+
                 return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
@@ -73,24 +87,37 @@ def profile(request):
         )
         if form.is_valid():
             form.save()
-            messages.success(request, f'Ваш профиль успешно обновлён!')
+            messages.success(request, f"Ваш профиль успешно обновлён!")
             return HttpResponseRedirect(reverse("user:profile"))
     else:
         form = ProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
     context = {
         "title": "Slastena - Личный кабинет",
         "form": form,
+        "orders": orders
     }
 
     return render(request, "users/profile.html", context)
 
 
 def users_cart(request):
-    return render(request, 'users/users_cart.html')
+    return render(request, "users/users_cart.html")
+
 
 @login_required()
 def logout(request):
-    messages.success(request, f'Ты это, {request.user.username}, заходи если что.')
+    messages.success(request, f"Ты это, {request.user.username}, заходи если что.")
     auth.logout(request)
     return redirect(reverse("main:index"))
